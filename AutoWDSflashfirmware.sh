a49c499cf003fa7a14779900e280857addb6a40a
Konstantin Manna <konstantin.manna@lancom.de>
2014-06-03 18:37:58 +0200

    turned off hostkeyverification

--- AutoWDSflashfirmware.sh
+++ AutoWDSflashfirmware.sh
@@ -20,62 +20,62 @@ do
 	case $i in
 	"11")
 		cp "$l322fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l322fw 172.16.40.121:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l322fw 172.16.40.121:firmware"
 		vzctl exec "$i" "rm /root/$l322fw"
 		;;
 	"12")
 		cp "$l322fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l322fw 172.16.40.122:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l322fw 172.16.40.122:firmware"
 		vzctl exec "$i" "rm /root/$l322fw"
 		;;
 	"13")
 		cp "$l322fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l322fw 172.16.40.123:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l322fw 172.16.40.123:firmware"
 		vzctl exec "$i" "rm /root/$l322fw"
 		;;
 	"14")
 		cp "$l452fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l452fw 172.16.40.111:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l452fw 172.16.40.111:firmware"
 		vzctl exec "$i" "rm /root/$l452fw"
 		;;
 	"15")
 		cp "$l452fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l452fw 172.16.40.112:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l452fw 172.16.40.112:firmware"
 		vzctl exec "$i" "rm /root/$l452fw"
 		;;
 	"16")
 		cp "$l452fw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $l452fw 172.16.40.113:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $l452fw 172.16.40.113:firmware"
 		vzctl exec "$i" "rm /root/$l452fw"
 		;;
 	"17")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.101:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.101:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	"18")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.102:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.102:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	"19")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.103:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.103:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	"20")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.104:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.104:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	"21")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.105:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.105:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	"22")
 		cp "$batfw" "/var/lib/vz/root/${i}/root/"
-		vzctl exec "$i" "scp $batfw 172.16.40.106:firmware"
+		vzctl exec "$i" "scp -oStrictHostKeyChecking=no $batfw 172.16.40.106:firmware"
 		vzctl exec "$i" "rm /root/$batfw"
 		;;
 	*)
