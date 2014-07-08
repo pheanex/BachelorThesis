d0b0b1daa9cddc0e64f1e7411155b8e9b6155c85
Konstantin Manna <konstantin.manna@lancom.de>
2014-07-08 13:33:31 +0200

    updated draw graph

--- Scripts/draw_graph.py
+++ Scripts/draw_graph.py
@@ -6,9 +6,51 @@ import matplotlib
 matplotlib.use('svg')
 import matplotlib.pyplot as plt
 import matplotlib.legend as lg
+from operator import itemgetter
+from pylab import *
 
 
-for reportfile in ["rx_crc_errors", "rx_errors", "tx_errors", "retries", "multiple_retries", "rx_packets", "tx_packets", "rx_bytes", "tx_bytes", "modem_load", "noise", "tx_discard"]:
+# Create artificial graphs from data
+def write_graph(title, ylabel, data1, data2, filename):
+    plt.title(title)
+    plt.ylabel(ylabel)
+    linestyle_iterator = 1
+    boxplotlist = list()
+    namelist = list()
+    for first_data, second_data in zip(sorted(data1, key=itemgetter(0)), sorted(data2, key=itemgetter(0))):
+        divisionlist = [int(a) * 1.0 / (int(b) + int(a)) for a, b in zip(first_data[1:], second_data[1:])]
+        data = divisionlist[:88]
+
+        # Convert data from 0.9 in 90 (Because numpy does seem to have problems with float)
+        data = [int(round(float(element) * 100)) for element in data]
+
+        plt.plot(xscale, data, linestyle[linestyle_iterator], label=first_data[0])
+        linestyle_iterator += 1
+        namelist.append(first_data[0])
+        boxplotlist.append(data)
+
+    lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0., title="Used Channels")
+    plt.grid(True)
+    plt.xlabel("Time in s")
+    plt.xlim(0, 600)
+    plt.savefig(filename + ".svg", bbox_inches='tight')
+    plt.close()
+
+    xticks([1, 2, 3, 4, 5], namelist)
+    boxplot(boxplotlist)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel("Used Channels")
+    savefig(filename + "_boxplot.svg", bbox_inches='tight')
+    clf()
+
+
+linestyle_iterator = 1
+linestyle = {1: "ob-", 2: "vg-", 3: "sr-", 4: "pc-", 5: "*m-"}
+boxplotlist = list()
+xscale = range(0, 614, 7)
+for reportfile in ["rx_errors", "tx_errors", "retries", "rx_packets", "tx_packets", "rx_bytes", "tx_bytes", "modem_load", "noise"]:
+    linestyle_iterator = 1
     f = open(reportfile, "r")
     filestring = f.read()
     f.close()
@@ -27,45 +69,46 @@ for reportfile in ["rx_crc_errors", "rx_errors", "tx_errors", "retries", "multip
     plt.figure(figsize=(7, 4))
 
     if reportfile == "rx_crc_errors":
-        plt.title("Receive CRC-Error Rate")
-        plt.ylabel("Receive CRC-Errors/s")
+        title = "Packet Receive CRC-Error Rate/s"
+        ylabel = "Receive CRC-Errors/s"
     elif reportfile == "rx_errors":
-        plt.title("Receive Error Rate")
-        plt.ylabel("Receive Errors/s")
+        title = "Received Packets with Errors/s"
+        ylabel = "Packets/s"
     elif reportfile == "tx_errors":
-        plt.title("Transmit Error Rate")
-        plt.ylabel("Transmit Errors/s")
+        title = "Undeliverable Packets/s"
+        ylabel = "Packet Transmit Errors/s"
     elif reportfile == "retries":
-        plt.title("Send Packet Retries")
-        plt.ylabel("Retries/s")
+        title = "Successfully Transmitted Packets (One Retry)/s"
+        ylabel = "Retries/s"
     elif reportfile == "multiple_retries":
-        plt.title("Multiple Send Packet Retries")
-        plt.ylabel("Send Packet Retries")
+        title = "Successfully Transmitted Packets (Multiple Retries)/s"
+        ylabel = "Packets/s"
     elif reportfile == "rx_packets":
-        plt.title("Received Packet Rate ")
-        plt.ylabel("Received Packets/s")
+        title = "Successfully Received Packets/s"
+        ylabel = "Packets/s"
     elif reportfile == "tx_packets":
-        plt.title("Transmit Packet Rate")
-        plt.ylabel("Transmitted Packets/s")
+        title = "Successfully Transmitted Packets/s"
+        ylabel = "Packets/s"
     elif reportfile == "rx_bytes":
-        plt.title("Received Byte Rate")
-        plt.ylabel("Bytes Received/s")
+        title = "Successfully Received Bytes/s"
+        ylabel = "KBytes/s"
     elif reportfile == "tx_bytes":
-        plt.title("Transmit Byte Rate")
-        plt.ylabel("Transmitted Bytes/s")
+        title = "Successfully Transmitted Bytes/s"
+        ylabel = "KBytes/s"
     elif reportfile == "modem_load":
-        plt.title("Modem Load")
+        title = "Average Modem Load (Per AP)"
+        ylabel = "% Medium used"
     elif reportfile == "noise":
-        plt.title("Noise Level")
-        plt.ylabel("dBm")
+        title = "Average Noise Level (Per AP)"
+        ylabel = "dBm"
     elif reportfile == "tx_discard":
-	plt.title("Discarded Packets")
-	plt.ylabel("Number of discarded Packets / s")
+        title = "Discarded Packets/s"
+        ylabel = "Packets/s"
     else:
         print("Error: unknown reportfile")
         exit(1)
 
-    sum7_divide_group = {"rx_crc_errors", "rx_errors", "tx_errors", "retries", "multiple_retries", "rx_packets", "tx_packets", "rx_bytes", "tx_bytes", "tx_discard"}
+    sum7_divide_group = {"rx_crc_errors", "rx_errors", "tx_errors", "retries", "rx_packets", "tx_packets", "rx_bytes", "tx_bytes", "tx_discard"}
 
     if reportfile == "rx_packets":
         rx_packets = masterlist
@@ -77,54 +120,53 @@ for reportfile in ["rx_crc_errors", "rx_errors", "tx_errors", "retries", "multip
         retries = masterlist
     elif reportfile == "multiple_retries":
         multiple_retries = masterlist
+    elif reportfile == "tx_errors":
+        tx_errors = masterlist
 
     # Cycle through the data sets
-    for listentry in masterlist:
-        if reportfile in sum7_divide_group:
-            seconds_list = [int(x)/7 for x in listentry[1:]]
-            plt.plot(seconds_list, label=listentry[0])
-        else:
-            plt.plot(listentry[1:], label=listentry[0])
+    namelist = list()
+    boxplot_data_list = list()
+    for listentry in sorted(masterlist, key=itemgetter(0)):
 
-    lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0.)
+        # Limit plot to 600s (since the tests only ran that long and everything after that is garbage/artefacts)
+        plt.xlim(0, 600)
+
+        # Remove the title from data
+        data = listentry[1:89]
+
+        namelist.append(listentry[0])
+
+        if reportfile in sum7_divide_group:
+            # Divide data by 7 since we only have 7 seconds intervals
+            data = [int(x)/7 for x in listentry[1:89]]
+            if reportfile == "rx_bytes" or reportfile == "tx_bytes":
+                # Divide by 1000 so we get KBytes instead of Bytes
+                data = [x/(1000 * 1.0) for x in data]
+
+        # Round data for boxplot
+        rounded_data = [int(round(float(element))) for element in data]
+
+        boxplot_data_list.append(rounded_data)
+        plt.plot(xscale, data, linestyle[linestyle_iterator], label=listentry[0])
+        linestyle_iterator += 1
+
+    # Graph
+    lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0., title="Used Channels")
+    plt.title(title)
+    plt.ylabel(ylabel)
     plt.grid(True)
-    plt.xlabel("Time")
+    plt.xlabel("Time in s")
     plt.savefig(reportfile + ".svg", bbox_inches='tight')
     plt.close()
 
-# Create the extra graphs
-# Cycle through the data sets
-plt.title("Receive Error Rate / Received Packet Rate")
-plt.ylabel("% Packet Errors of Received Packets")
-for rx_errors_sublist, rxpackets_sublist in zip(rx_errors, rx_packets):
-    divisionlist = [int(a) * 1.0 / int(b) for a, b in zip(rx_errors_sublist[1:], rxpackets_sublist[1:])]
-    plt.plot(divisionlist, label=rx_errors_sublist[0])
-lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0.)
-plt.grid(True)
-plt.xlabel("Time")
-plt.savefig("recpackerr.svg", bbox_inches='tight')
-plt.close()
+    # Boxplot
+    xticks([1, 2, 3, 4, 5], namelist)
+    boxplot(boxplot_data_list)
+    plt.title(title)
+    plt.ylabel(ylabel)
+    plt.xlabel("Used Channels")
+    savefig(reportfile + "_boxplot.svg", bbox_inches='tight')
+    clf()
 
-plt.title("Send Error Rate / Send Packet Rate")
-plt.ylabel("% Packet Errors of Sent Packets")
-for rx_errors_sublist, rxpackets_sublist in zip(retries, tx_packets):
-    divisionlist = [int(a) * 1.0 / int(b) for a, b in zip(rx_errors_sublist[1:], rxpackets_sublist[1:])]
-    plt.plot(divisionlist, label=rx_errors_sublist[0])
-lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0.)
-plt.grid(True)
-plt.xlabel("Time")
-plt.savefig("sentpackerr.svg", bbox_inches='tight')
-plt.close()
-
-plt.title("Multiple Send Error Rate / Send Packet Rate")
-plt.ylabel("% Multi Packet Errors of Sent Packets")
-for rx_errors_sublist, rxpackets_sublist in zip(multiple_retries, tx_packets):
-    divisionlist = [int(a) * 1.0 / int(b) for a, b in zip(rx_errors_sublist[1:], rxpackets_sublist[1:])]
-    plt.plot(divisionlist, label=rx_errors_sublist[0])
-lg = plt.legend(loc=2, prop={"size": 10}, bbox_to_anchor=(1.05, 0.7), borderaxespad=0.)
-plt.grid(True)
-plt.xlabel("Time")
-plt.savefig("multisentpackerr.svg", bbox_inches='tight')
-plt.close()
-
-
+write_graph("Received Packets with Errors / All Received Packets", "%", rx_errors, rx_packets, "recpackerr")
+write_graph("Packet Transmission Retries / All Transmitted Packets", "%", retries, tx_packets, "sentpackerr")
