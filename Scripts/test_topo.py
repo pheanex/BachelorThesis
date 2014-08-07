c4613401abbd8b81d075dbaf55a3bc804822efbe
Konstantin Manna <konstantin.manna@lancom.de>
2014-08-07 15:31:13 +0200

    Code: renaming stuff

--- Scripts/test_topo.py
+++ Scripts/test_topo.py
@@ -16,7 +16,15 @@ for i in range(9):
         g.add_edge(i, module_name)
         g.edge[i][module_name]["snr"] = max_weight
 
-seen_links = {0: [0, 1, 3, 4], 1: [0, 1, 2, 3, 4, 5], 2: [1, 2, 4, 5], 3: [0, 1, 3, 4, 6, 7], 4: [0, 1, 2, 3, 4, 5, 6, 7, 8], 5: [1, 2, 4, 5, 7, 8], 6: [3, 4, 6, 7], 7: [3, 4, 5, 6, 7, 8], 8: [4, 5, 7, 8]}
+seen_links = {0: [0, 1, 3, 4],
+              1: [0, 1, 2, 3, 4, 5],
+              2: [1, 2, 4, 5],
+              3: [0, 1, 3, 4, 6, 7],
+              4: [0, 1, 2, 3, 4, 5, 6, 7, 8],
+              5: [1, 2, 4, 5, 7, 8],
+              6: [3, 4, 6, 7],
+              7: [3, 4, 5, 6, 7, 8],
+              8: [4, 5, 7, 8]}
 
 for node_a in seen_links:
     for node_b in seen_links[node_a]:
@@ -28,35 +36,33 @@ for node_a in seen_links:
                 node_b = str(node_b)
                 moda = node_a + "." + i
                 modb = node_b + "." + j
-
                 if moda == modb:
                     continue
-
                 g.add_edge(moda, modb, snr=random.randint(30, 96))
 
 tcca.write_json(g, "/home/kmanna/autowdsstatus/autowds-graph.json")
 time.sleep(2)
 webbrowser.open("file:///home/kmanna/autowdsstatus/autowds-graph.html")
 
-mst_graph = tcca.calculate_mst(g)
+mst_graph = tcca.calculate_st(g)
 time.sleep(2)
 tcca.write_json(mst_graph, "/home/kmanna/autowdsstatus/autowds-graph.json")
 time.sleep(2)
 webbrowser.open("file:///home/kmanna/autowdsstatus/autowds-graph.html")
 
-robust_graph = tcca.calculate_backup_links(mst_graph, g)
+robust_graph = tcca.calculate_survival_links(mst_graph, g)
 time.sleep(2)
 tcca.write_json(robust_graph, "/home/kmanna/autowdsstatus/autowds-graph.json")
 time.sleep(2)
 webbrowser.open("file:///home/kmanna/autowdsstatus/autowds-graph.html")
 
-ca_mst_graph = tcca.calculate_caa_for_graph(mst_graph, g, [1,6,11])
+ca_mst_graph = tcca.calculate_ca(mst_graph, g, [1,6,11])
 time.sleep(2)
 tcca.write_json(ca_mst_graph, "/home/kmanna/autowdsstatus/autowds-graph.json")
 time.sleep(2)
 webbrowser.open("file:///home/kmanna/autowdsstatus/autowds-graph.html")
 
-ca_robust_graph = tcca.calculate_caa_for_graph(robust_graph, g, [1,6,11])
+ca_robust_graph = tcca.calculate_ca(robust_graph, g, [1,6,11])
 time.sleep(2)
 tcca.write_json(ca_robust_graph, "/home/kmanna/autowdsstatus/autowds-graph.json")
 time.sleep(2)
